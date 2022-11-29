c1d2e84d143c5edf9e7a5deac474d310c867967e
Meshal <meshalalanazinsa@gmail.com>
2022-11-29 23:35:51 +0300

    formatting, changed datatype

--- migrations/sqls/20221129181030-create-products-table-up.sql
+++ migrations/sqls/20221129181030-create-products-table-up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS products (
     product_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL, 
-    price VARCHAR(100) NOT NULL, 
+    price FLOAT NOT NULL, 
     category VARCHAR(100) 
 );
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
+
 const app: express.Application = express();
 const address = '127.0.0.1:3000';
 
 app.use(bodyParser.json());
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
